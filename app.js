84a5e1a6a76cb24a5428196ee44229dea0808494
WildanUmam <wildanumam40@gmail.com>
2023-12-23 13:10:39 +0700

    Menyelesaikan revisi

--- app.js
+++ app.js
@@ -1,37 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
-const MenuController = require("./controller/menuController");
-const OrderController = require("./controller/orderController");
-const OrderDetailController = require("./controller/orderDetailController");
-
 const routers = require("./routers/index");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 4005;
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { 
   res.send("<h1>Hello World!</h1>");
 });
 
 app.use(routers);
-
-// <----------MENU---------->
-// Get menu list
-
-
-
-// <----------Order---------->
-//Read Order list
-
-
-
-//<---------Order Detail---------->
-
 
 
 app.listen(port, () => {
